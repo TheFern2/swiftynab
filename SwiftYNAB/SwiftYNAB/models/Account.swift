6ce9aea9272f2faca51e2e480d1229627b803856
Fernando Balandran <fernandobe+git@protonmail.com>
2024-11-23 10:44:32 -0600

    init

--- SwiftYNAB/SwiftYNAB/models/Account.swift
+++ SwiftYNAB/SwiftYNAB/models/Account.swift
@@ -63,4 +63,45 @@ public struct Account: Codable, Equatable {
 
     /// Deleted account or not
     public let deleted: Bool
+
+    /// Initializer for the `Account` struct
+    public init(
+        id: String,
+        name: String,
+        type: String,
+        onBudget: Bool,
+        closed: Bool,
+        note: String?,
+        balance: Int,
+        clearedBalance: Int,
+        unclearedBalance: Int,
+        transferPayeeId: String,
+        directImportLinked: Bool,
+        directImportInError: Bool,
+        lastReconciledAt: String?,
+        debtOriginalBalance: Int?,
+        debtInterestRates: [String: Int],
+        debtMinimumPayments: [String: Int],
+        debtEscrowAmounts: [String: Int],
+        deleted: Bool
+    ) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.onBudget = onBudget
+        self.closed = closed
+        self.note = note
+        self.balance = balance
+        self.clearedBalance = clearedBalance
+        self.unclearedBalance = unclearedBalance
+        self.transferPayeeId = transferPayeeId
+        self.directImportLinked = directImportLinked
+        self.directImportInError = directImportInError
+        self.lastReconciledAt = lastReconciledAt
+        self.debtOriginalBalance = debtOriginalBalance
+        self.debtInterestRates = debtInterestRates
+        self.debtMinimumPayments = debtMinimumPayments
+        self.debtEscrowAmounts = debtEscrowAmounts
+        self.deleted = deleted
+    }
 }
